54d4dcc2a703a8b4d2af21d1b45d27458b610c3e
y3nu5 <y3nusmuhammad@gmail.com>
2026-06-13 22:54:29 +0800

    fix: remove vercel build artifacts from git, update gitignore

--- backend/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.C9CyYn3y.js","_app/immutable/chunks/BBx9jlSs.js","_app/immutable/chunks/BExlx1mT.js","_app/immutable/chunks/CC4oXkAW.js","_app/immutable/chunks/DZTE4pDc.js"];
-export const stylesheets = [];
-export const fonts = [];
